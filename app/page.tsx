5b061332f60ae40feb5209180f247d9b474c45cb
Andreas <andreas.kibsgaard@icloud.com>
2026-04-22 13:10:07 +0200

    ordna bilde på fremsiden

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <main>E-Sport Nettside Shope</main>
-      <div><h1><section className="relative w-full h-screen"><img src="tiller-esport-logo.png" alt="Shope" /></section></h1></div>
-    </div>
+    <main className="relative w-full h-screen">
+      <Image
+        src="/image0.jpeg"
+        alt="Shope"
+        fill
+        className="h-screen overflow-y-hidden md:overflow-y-auto"
+      />
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h1 className="text-white text-4xl font-bold">
+        </h1>
+      </div>
+    </main>
   );
 }
