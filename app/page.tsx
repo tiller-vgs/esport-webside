b5c2ebe8c5d1f22d0d38130357e10273089c3b77
Andreas <andreas.kibsgaard@icloud.com>
2026-04-22 13:23:11 +0200

    fiksa bilde posisjonen

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,15 @@ export default function Home() {
         src="/image0.jpeg"
         alt="Shope"
         fill
-        className="h-screen overflow-y-hidden md:overflow-y-auto"
+        className="object-cover object-[center_140%]"
       />
 
       <div className="absolute inset-0 flex items-center justify-center">
         <h1 className="text-white text-4xl font-bold">
         </h1>
+        
       </div>
+      
     </main>
   );
 }
