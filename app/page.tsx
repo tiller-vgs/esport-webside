8d9cba0b953fed76b0be26c95801e63d65c30fab
Andreas <andreas.kibsgaard@icloud.com>
2026-04-22 12:53:20 +0200

    første commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <main>E-Sport Nettside</main>
+      <main>E-Sport Nettside Shope</main>
+      <div><h1><section className="relative w-full h-screen"><img src="tiller-esport-logo.png" alt="Shope" /></section></h1></div>
     </div>
   );
 }
