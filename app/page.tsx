613ed44f247228bfbdb72bf851f7ca16144697b0
Andreas <andreas.kibsgaard@icloud.com>
2026-04-27 10:53:38 +0200

    La inn advertisment for tiller esporten nederst i home pagen med siste nyheter kommeende turneringer og Våre lag

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,39 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative w-full h-screen">
+    <main className="flex flex-col">
+    <div className="relative w-full h-screen">
       <Image
         src="/image0.jpeg"
-        alt="Shope"
+        alt="Shof E sport"
         fill
         className="object-cover object-[center_140%]"
       />
-
-      <div className="absolute inset-0 flex items-center justify-center">
-        <h1 className="text-white text-4xl font-bold">
-        </h1>
-        
       </div>
-      
+      <section className="bg-black text-white py-12 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          
+          <div>
+            <h2 className="text-sm uppercase">
+              Siste Nyheter
+            </h2>
+          </div>
+
+          <div>
+            <h2 className="text-sm uppercase">
+              Kommende Turneringer
+            </h2>
+          </div>
+
+          <div>
+            <h2 className="text-sm uppercase">
+              Våre Lag
+            </h2>
+          </div>
+
+        </div>
+      </section>
+
     </main>
   );
 }
